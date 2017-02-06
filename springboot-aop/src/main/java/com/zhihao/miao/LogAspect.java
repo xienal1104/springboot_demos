b2597b2930729230a6d3975ead33f76489ebcd2c
miaozhihao001 <1026145686@qq.com>
2017-02-06 23:19:12 +0800

    springboot-aop/

---
+++ springboot-aop/src/main/java/com/zhihao/miao/LogAspect.java
@@ -0,0 +1,8 @@
+package com.zhihao.miao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogAspect {
+
+}
---
+++ springboot-aop/src/main/java/com/zhihao/miao/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.zhihao.miao.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDao {
+
+}

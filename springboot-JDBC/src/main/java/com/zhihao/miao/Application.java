409404d506281705943dd8fd6fc4faea3fc85e09
miaozhihao001 <1026145686@qq.com>
2017-02-06 23:20:17 +0800

    springboot-JDBC/

--- springboot-JDBC/src/main/java/com/zhihao/miao/Application.java
+++ springboot-JDBC/src/main/java/com/zhihao/miao/Application.java
@@ -1,26 +1,27 @@
 package com.zhihao.miao;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		//services.msc
 		DataSource ds = context.getBean(DataSource.class);
+		//org.apache.tomcat.jdbc.pool.DataSource@36328d33,默认的是tomcat数据源
 		System.out.println(ds);
 		Connection conn = ds.getConnection();
 		System.out.println(conn.getCatalog());
 		conn.close();
-		System.out.println(context.getBean(JdbcTemplate.class));
+		//System.out.println(context.getBean(JdbcTemplate.class));
 		context.close();
 	}
 }

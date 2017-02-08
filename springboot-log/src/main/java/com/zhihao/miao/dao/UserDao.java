be1ab11e8316ccf66dced798227577b9c4faec38
miaozhihao001 <1026145686@qq.com>
2017-02-08 23:28:07 +0800

    springboot-log/

---
+++ springboot-log/src/main/java/com/zhihao/miao/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.zhihao.miao.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDao {
+	private Logger log = LoggerFactory.getLogger(UserDao.class);
+	
+	public void log(){
+		log.debug("user dao debug log");
+		log.info("user dao info log");
+		log.warn("user dao warn log");
+		log.error("user dao error log");
+	}
+}

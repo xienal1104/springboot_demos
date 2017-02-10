814300f314f4c149b0ddb482e7ef821420920060
zhihao.miao <zhihao.miao@ele.me>
2017-02-10 18:33:30 +0800

    springboot-actuator/

--- springboot-actuator/src/main/java/com/zhihao/miao/Application.java
+++ springboot-actuator/src/main/java/com/zhihao/miao/Application.java
@@ -2,8 +2,12 @@ package com.zhihao.miao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=WebSocketAutoConfiguration.class)
+@PropertySource(value="classpath:my.properties")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);

88eb27f59ef683172c97e1c05739284bf64ed2db
miaozhihao001 <1026145686@qq.com>
2017-02-12 22:37:14 +0800

    springboot-JDBC/

--- springboot-JDBC/src/main/java/com/zhihao/miao/Application.java
+++ springboot-JDBC/src/main/java/com/zhihao/miao/Application.java
@@ -1,5 +1,7 @@
 package com.zhihao.miao;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,9 +13,9 @@ public class Application {
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		//services.msc
-		//DataSource ds = context.getBean(DataSource.class);
+		DataSource ds = context.getBean(DataSource.class);
 		//org.apache.tomcat.jdbc.pool.DataSource,默认的是tomcat数据源
-		//System.out.println(ds.getClass().getName());
+		System.out.println(ds.getClass().getName());
 		//Connection conn = ds.getConnection();
 		//System.out.println(conn.getCatalog());
 		//conn.close();
@@ -22,7 +24,7 @@ public class Application {
 		//System.out.println(context.getBean(JdbcTemplate.class));
 		
 		//context.getBean(ProductDao.class).addProductBatch(new String[]{"daopian","cup","card"});
-		context.getBean(ProductDao.class).addProductBatch(new String[]{"shouji","miaozhihao","maojin"});
-		context.close();
+//		context.getBean(ProductDao.class).addProductBatch(new String[]{"shouji","miaozhihao","maojin"});
+//		context.close();
 	}
 }
